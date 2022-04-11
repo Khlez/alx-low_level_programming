929a7203dc109f18cf03cd43eeb26348ea3f91bd
Khlez <klazyk.ec@gmail.com>
2022-04-11 06:36:24 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,13 @@
 */
 int main(void)
 {
-	char i;	
+char i;	
 
-		for (i = 'a'; i <= 'z'; i++)
-		{	
-		if (i != 'e' && i != 'q')
-			putchar(i);
-		}		
-		putchar('\n');
-		return (0);
+for (i = 'a'; i <= 'z'; i++)
+{	
+	if (i != 'e' && i != 'q')
+		putchar(i);
+}		
+putchar('\n');
+return (0);
 }
