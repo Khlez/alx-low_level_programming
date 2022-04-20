2a39e1d41e797ddeb540d283c46fa45a5af9cbd6
Khlez <klazyk.ec@gmail.com>
2022-04-20 12:48:48 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,17 @@
 #include "main.h"
+
 /**
 * _puts- print a string followed by a new line
 * @str: string to print
 */
+
 void _puts(char *str)
 {
-	printf("%s\n", *str);
-	return (0);
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('/n');
 }
