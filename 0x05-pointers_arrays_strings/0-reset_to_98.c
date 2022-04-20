d14d44b6ebe70b2fbf38dcedfc2d61f8f339ea12
Khlez <klazyk.ec@gmail.com>
2022-04-20 10:18:26 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
-*main - Entry point
-*
-* Return - 0
+* reset_to_98 - updates the value *n points to.
+* @n: the number to modify.
 */
 void reset_to_98(int *n)
 {
-	int *n = 100;
+	*n = 98;
 }
-
