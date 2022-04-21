5f6ac81c3d4ebdadbfec4b350d469e6d3ef6d678
Khlez <klazyk.ec@gmail.com>
2022-04-21 06:13:05 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* rev_string - a function that reverses a string
+* @s: string to reverse
+*/
+void rev_string(char *s)
+{
+	int j = 0;
+	int reverse = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		reverse++;
+	}
+}
